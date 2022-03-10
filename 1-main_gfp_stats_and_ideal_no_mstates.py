c373e3a2ad0753e5f09d0bcc476ffa05b5379331
Nikola Jajcay <nikola.jajcay@gmail.com>
2022-03-10 14:55:30 +0100

    more criteria for ideal number of microstates

--- 1-main_gfp_stats_and_ideal_no_mstates.py
+++ 1-main_gfp_stats_and_ideal_no_mstates.py
@@ -5,13 +5,19 @@ Compute GFP peak stats and test for ideal number of microstates.
 """
 import os
 from copy import deepcopy
-from multiprocessing import Pool, cpu_count
 
 import numpy as np
 import pandas as pd
-from src.helpers import DATA_ROOT, RESULTS_ROOT, make_dirs, set_logger
+
+import src.clustering_scores as scores
+from src.helpers import (
+    DATA_ROOT,
+    RESULTS_ROOT,
+    make_dirs,
+    run_in_parallel,
+    set_logger,
+)
 from src.recording import load_all_data
-from tqdm import tqdm
 
 # 4: PSI-T3 - no data
 # 13: PSI-T3 - lot of artefacts
@@ -20,31 +26,11 @@ from tqdm import tqdm
 # 22: PSI-T1 - very short data
 EXCLUDE_SUBJECTS = [4, 13, 14, 20, 22]
 
-WORKERS = cpu_count()
-NO_STATES_RANGE = np.arange(1, 11)
+WORKERS = 5
+NO_STATES_RANGE = np.arange(2, 11)
 FILTER_OPTIONS = [(2.0, 20.0), (1.0, 40.0)]
-# number of initialisation for each microstate computation
-N_INITS = 500
-
-
-def pm_variance_test(gev, no_states, n_channels):
-    """
-    Compute variance test according to Pascual-Marqui, R. D., Michel, C. M., &
-    Lehmann, D. (1995). Segmentation of brain electrical activity into
-    microstates: model estimation and validation. IEEE Transactions on
-    Biomedical Engineering, 42(7), 658-665 ~ eq. (20).
-
-    :param gev: global `unexplained` (i.e. 1 - explained variance) variance by
-        microstate decomposition
-    :type gev: float
-    :param no_states: number of canonical states for decomposition
-    :type no_states: int
-    :param n_channels: number of channels in EEG recording
-    :type n_channels: int
-    """
-    return gev * np.power(
-        (1.0 / (n_channels - 1)) * (n_channels - 1 - no_states), -2
-    )
+# number of initialisations for each microstate computation
+N_INITS = 200
 
 
 def _process_recording(args):
@@ -55,28 +41,37 @@ def _process_recording(args):
         tuple
     :type args: tuple[`PsilocybinRecording` & tuple]
     """
-    recording, filter_ = args
-    out = [recording.subject, recording.session, filter_]
+    recording, filter_, n_states = args
+    df = {
+        "subject": recording.subject,
+        "session": recording.session,
+        "filter": filter_,
+    }
     n_channels = recording.info["nchan"]
     recording.preprocess(filter_[0], filter_[1])
     recording.gfp()
-    out.append(len(recording.gfp_peaks))
-    for n_states in NO_STATES_RANGE:
-        recording.run_microstates(n_states=n_states, n_inits=N_INITS)
-        out += [
-            pm_variance_test(
-                gev=1.0 - recording.gev_tot,
-                no_states=n_states,
-                n_channels=n_channels,
-            ),
-            pm_variance_test(
-                gev=1.0 - recording.gev_gfp,
-                no_states=n_states,
-                n_channels=n_channels,
-            ),
-        ]
-    assert len(out) == 4 + 2 * len(NO_STATES_RANGE)
-    return out
+    df["# GFP peaks"] = len(recording.gfp_peaks)
+
+    recording.run_microstates(n_states=n_states, n_inits=N_INITS)
+    df["# states"] = n_states
+    df["PM variance total"] = scores.pascual_marqui_variance_test(
+        1.0 - recording.gev_tot, n_states, n_channels
+    )
+    df["PM variance GFP"] = scores.pascual_marqui_variance_test(
+        1.0 - recording.gev_gfp, n_states, n_channels
+    )
+    df["Davies-Bouldin"] = scores.davies_bouldin_test(
+        recording.data.T, recording.segmentation
+    )
+    df["Dunn"] = scores.dunn_test(recording.data.T, recording.segmentation)
+    df["Silhouette"] = scores.silhouette_test(
+        recording.data.T, recording.segmentation
+    )
+    df["Calinski-Harabasz"] = scores.calinski_harabasz_test(
+        recording.data.T, recording.segmentation
+    )
+
+    return pd.DataFrame(df)
 
 
 def main():
@@ -86,32 +81,18 @@ def main():
     data = load_all_data(os.path.join(DATA_ROOT, "processed"), EXCLUDE_SUBJECTS)
     assert len(data) % 5 == 0, len(data)
 
-    pool = Pool(WORKERS)
-    results = pool.map(
+    results = run_in_parallel(
         _process_recording,
-        tqdm(
-            [
-                (deepcopy(recording), filter_)
-                for recording in data
-                for filter_ in FILTER_OPTIONS
-            ]
-        ),
+        [
+            (deepcopy(recording), filter_, n_states)
+            for recording in data
+            for filter_ in FILTER_OPTIONS
+            for n_states in NO_STATES_RANGE
+        ],
+        workers=WORKERS,
     )
-    assert len(results) == 2 * len(data)
-    pool.close()
-    pool.join()
 
-    results = pd.DataFrame(
-        results,
-        columns=["subject", "session", "filter", "# GFP peaks"]
-        + sum(
-            [
-                [f"sigma_MCV total: {i} mstates", f"sigma_MCV GFP: {i} mstates"]
-                for i in NO_STATES_RANGE
-            ],
-            [],
-        ),
-    )
+    results = pd.concat(list(results), axis=0)
     results.to_csv(os.path.join(working_folder, "gfp_peaks_var_test.csv"))
 
 
--- src/helpers.py
+++ src/helpers.py
@@ -7,7 +7,10 @@ Set of helpers.
 import logging
 import os
 import time
-from functools import wraps
+from functools import partial, wraps
+from multiprocessing import Pool, cpu_count
+
+from tqdm import tqdm
 
 PROJECT_ROOT = os.path.normpath(
     os.path.join(os.path.abspath(__file__), "..", "..", "..")
@@ -20,6 +23,68 @@ PLOTS_ROOT = os.path.join(PROJECT_ROOT, "results_figs")
 
 LOG_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 LOG_EXT = ".log"
+
+
+def run_in_parallel(
+    partial_function,
+    iterable,
+    workers=cpu_count(),
+    length=None,
+    assert_ordered=False,
+):
+    """
+    Wrapper for running functions in parallel with tqdm bar.
+
+    :param partial_function: partial function to be evaluated
+    :type partial_function: :class:`_functools.partial`
+    :param iterable: iterable comprised of arguments to be fed to partial
+        function
+    :type iterable: iterable
+    :param workers: number of workers to be used
+    :type workers: int
+    :param length: Length of the iterable / generator.
+    :type length: int|None
+    :param assert_ordered: whether to assert order of results same as the
+        iterable (imap vs imap_unordered)
+    :type assert_ordered: bool
+    :return: list of values returned by partial function
+    :rtype: list
+    """
+    total = length
+    if total is None:
+        try:
+            total = len(iterable)
+        except (TypeError, AttributeError):
+            pass
+
+    # wrap method in order to get original exception from a worker process
+    partial_function = partial(_worker_fn, fn=partial_function)
+
+    pool = Pool(workers)
+    imap_func = pool.imap_unordered if not assert_ordered else pool.imap
+    results = []
+    for result in tqdm(imap_func(partial_function, iterable), total=total):
+        results.append(result)
+    pool.close()
+    pool.join()
+
+    return results
+
+
+def _worker_fn(item, fn):
+    """
+    Wrapper for worker method in order to get original exception from
+    a worker process and to log correct exception stacktrace.
+
+    :param item: item from iterable
+    :param fn: partial function to be evaluated
+    :type fn: :class:`_functools.partial`
+    """
+    try:
+        return fn(item)
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 
 def set_logger(log_filename=None, log_level=logging.INFO):
